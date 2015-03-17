2918dc68e009f8647e40656de5b6cf3ace800547
Eiji Mirai <eiji.mirai@gmail.com>
2015-03-17 20:56:10 +0300

    Controller Articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,9 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all.page(params[:page]).per(3)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'main#index'
 
   resources :news
+  resources :articles
 
   post 'comment', to: 'news#comment'
 
